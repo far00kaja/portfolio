ed71a3c18a4076a7d0f560d47c205e34fa0a2db9
Dicky Darmawan <dicky.darmawan@aksesciptasolusi.com>
2024-03-15 13:29:03 +0700

    change href tolinkedin

--- src/pages/introduction.tsx
+++ src/pages/introduction.tsx
@@ -10,7 +10,7 @@ export default function Introduction() {
                 <h1 className="text-bold text-6xl py-10 text-justify break-words">👋 Hi,</h1>
                 <ul className="flex gap-4  flex-col font-semibold text-2xl text-gray-800 dark:text-gray-50 ">
                     <li>
-                        I’m <a href="https://github.com/far00kaja" className="underline" target="_blank">Dicky Darmawan</a>
+                        I’m <a href="https://www.linkedin.com/in/dicky-darmawan-cv/" className="underline" target="_blank">Dicky Darmawan</a>
                     </li><li>
                         - 👀 I’m interested and on exploration phase in depth about backend  </li>
                     <li>
