4d55bf608776eb9d1cdb5d52ca2b6021952ca17e
Dicky Darmawan <dicky.darmawan@aksesciptasolusi.com>
2024-04-18 11:41:12 +0700

    show navigation when mobile mode

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -30,24 +30,19 @@ export default function Navbar() {
             </header>
             <button
                 onClick={() => isShow(!show)}
-                className={`fixed m-4 right-0 bottom-0 z-50 bg-gray-800 focus:bg-gray-200  p-4 rounded-full block sm:hidden transition-all duration-300 delay-75 ease-linear`}>
+                className={`fixed m-4 right-0 bottom-0 z-50 dark:bg-gray-200 bg-gray-800 p-4 rounded-full block sm:hidden transition-all duration-300 delay-75 ease-linear`}>
                 {show ? (
-                    <>
-                        <svg fill="#000000" height="32px" width="32px" version="1.1" id="Capa_1"
-                            viewBox="0 0 490 490">
-                            <polygon points="11.387,490 245,255.832 478.613,490 489.439,479.174 255.809,244.996 489.439,10.811 478.613,0 245,234.161 
+                    <svg fill="#000000" height="32px" width="32px" version="1.1" id="Capa_1"
+                        viewBox="0 0 490 490">
+                        <polygon points="11.387,490 245,255.832 478.613,490 489.439,479.174 255.809,244.996 489.439,10.811 478.613,0 245,234.161 
 	11.387,0 0.561,10.811 234.191,244.996 0.561,479.174 "/>
-                        </svg>
-                    </>
+                    </svg>
                 ) : (
-                    <>
-                        <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path className="stroke-gray-200 hover:stroke-gray-400 dark:stroke-gray-800 |" d="M4 18L20 18" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
-                            <path className="stroke-gray-200 hover:stroke-gray-400 dark:stroke-gray-800 |" d="M4 18L20 18" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
-                            <path className="stroke-gray-200 hover:stroke-gray-400 dark:stroke-gray-800" d="M4 12L20 12" strokeWidth="2" strokeLinecap="round" />
-                            <path className="stroke-gray-200 hover:stroke-gray-400 dark:stroke-gray-800" d="M4 6L20 6" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
-                        </svg>
-                    </>
+                    <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path className="stroke-gray-200 hover:stroke-gray-200 dark:stroke-gray-800" d="M4 18L20 18" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+                        <path className="stroke-gray-200 hover:stroke-gray-200 dark:stroke-gray-800" d="M4 12L20 12" strokeWidth="2" strokeLinecap="round" />
+                        <path className="stroke-gray-200 hover:stroke-gray-200 dark:stroke-gray-800" d="M4 6L20 6" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+                    </svg>
                 )
                 }
 
